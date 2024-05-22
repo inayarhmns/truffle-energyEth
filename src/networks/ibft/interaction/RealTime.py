a8a45e9b1c8446de8fc5b61c7a407a5d89a64798
syifaafra <afra.mumt@gmail.com>
2024-05-22 08:38:53 +0700

    melanjutkan refactor RealTime.py

--- src/networks/ibft/interaction/RealTime.py
+++ src/networks/ibft/interaction/RealTime.py
@@ -8,7 +8,6 @@ import match
 import time
 import numpy as np
 import matplotlib.pyplot as plt
-import FlexCoin
 from web3.middleware import geth_poa_middleware
 
 # Compile and deploy in populus in one terminal and testrpc-py in another
@@ -23,12 +22,20 @@ jsonFile.close()
 
 abi = values['abi']
 address = input("What is the contract address? - RealTime: ")
-RealTime = web3.eth.contract(address, abi = abi)
+RealTime = web3.eth.contract(address=address, abi = abi)
+
+# Load and parse the ABI for FlexCoin
+jsonFileFlexCoin = open('./build/contracts/FlexCoin.json', 'r')
+valuesFlexCoin = json.load(jsonFileFlexCoin)
+jsonFileFlexCoin.close()
+abiFlexCoin = valuesFlexCoin['abi']
+addressFlexCoin = input("What is the contract address? - FlexCoin: ")
+FlexCoin = web3.eth.contract(address=addressFlexCoin, abi=abiFlexCoin)
 
 createNodeCost = []
 
 # Set how many nodes that should be simulated
-testRange = range(600, 601, 1)
+testRange = range(4, 5, 1)
 
 averageNode = [0 for i in testRange]
 averageCentral = [0 for i in testRange]
@@ -36,38 +43,56 @@ numTxCentral = [0 for i in testRange]
 numTxNodes = [0 for i in testRange]
 numTxInitiate = [0 for i in testRange]
 
-def setPrice(battery):
+def wait_for_receipt(tx_hash, timeout=120):
 # This function sets a price for the battery energy - i.e agent market decisions
+    start_time = time.time()
+    while True:
+        try:
+            receipt = web3.eth.get_transaction_receipt(tx_hash)
+            if receipt is not None:
+                return receipt
+        except:
+            pass
+        time.sleep(1)
+        if time.time() - start_time > timeout:
+            raise TimeoutError(f"Transaction {tx_hash} not confirmed in {timeout} seconds")
+def setPrice(battery, numHouses):
+    """
+    This function sets a price for the battery energy - i.e agent market decisions.
+    The price is set under 470 if the battery buys energy, and above 470 if the battery sells energy.
+    """
+    price = [[0 for _ in range(numHouses)] for _ in range(2)]
 
-    price = [[0 for x in range(numHouses)] for y in range(2)]
-    #price[0][0] = random.randint(480, 590)
-    #price[1][0] = random.randint(350, 460)
-
-    # The price is set under 470 if the battery buys energy, and above 470 if the battery sells energy.
     for i in range(0, 10, 2):
-        if (battery[i] <= 6700):
+        if battery[i] <= 6700:
             price[0][i] = random.randint(480, 590)
             price[1][i] = random.randint(350, 460)
         else:
             price[0][i] = random.randint(480, 590)
             price[1][i] = random.randint(350, 460)
+
+    print(f"Prices set for battery: {price}")
     return price
 
 def setFlexibility(battery, batteryFlag, numHouses):
-# Setting the different battery levels
+    """
+    This function sets the different battery levels.
+    5000 w in one hour = 5000 wh => 833 w max in 10 minutes
+    """
+    availableFlex = [[0 for _ in range(numHouses)] for _ in range(2)]
 
-    # 5000 w in one hour = 5000 wh => 833 w max in 10 minutes
-    availableFlex = [[0 for x in range(numHouses)] for y in range(2)]
-    for i in range(0,numHouses):
-        if (battery[i] > 840 and battery[i] < 12500):
+    for i in range(numHouses):
+        if 840 < battery[i] < 12500:
             availableFlex[0][i] = 700
             availableFlex[1][i] = 700
-        elif (battery[i] <= 840 and batteryFlag[i] == 1):
+        elif battery[i] <= 840 and batteryFlag[i] == 1:
             availableFlex[0][i] = 0
             availableFlex[1][i] = 700
-        elif (battery[i] >= 12500):
+        elif battery[i] >= 12500:
             availableFlex[0][i] = 700
             availableFlex[1][i] = 0
+
+    print(f"Flexibility set for battery: {availableFlex}")
     return availableFlex
 
 def trade(numTxCentral, numTxNodes, numTxInitiate, price, battery, availableFlex, deviation, numHouses):
@@ -75,42 +100,75 @@ def trade(numTxCentral, numTxNodes, numTxInitiate, price, battery, availableFlex
 
     wholesalePrice = 470
     flexFlag = -1
-    transactions = [[] for y in range(3)]
-    upPrice = [[0 for x in range(numHouses)] for y in range(2)]
+    transactions = [[] for _ in range(3)]
+    upPrice = [[0 for _ in range(numHouses)] for y in range(2)]
     upPrice[0] = [x for x in range(numHouses)]
     upPrice[1] = price[0]
-    downPrice = [[0 for x in range(numHouses)] for y in range(2)]
+    downPrice = [[0 for _ in range(numHouses)] for y in range(2)]
     downPrice[0] = [x for x in range(numHouses)]
     downPrice[1] = price[1]
-    upAvailableFlex = [[0 for x in range(numHouses)] for y in range(2)]
+    upAvailableFlex = [[0 for _ in range(numHouses)] for y in range(2)]
     upAvailableFlex[0] = [x for x in range(numHouses)]
     upAvailableFlex[1] = availableFlex[0] # This is used if the system needs less consumption
-    downAvailableFlex = [[0 for x in range(numHouses)] for y in range(2)]
+    downAvailableFlex = [[0 for _ in range(numHouses)] for y in range(2)]
     downAvailableFlex[0] = [x for x in range(numHouses)]
     downAvailableFlex[1] = availableFlex[1] # This is used if system needs more consumption
-    demand = [[] for y in range(2)]
-    supply = [[] for y in range(2)]
+    demand = [[] for _ in range(2)]
+    supply = [[] for _ in range(2)]
     updateCost = []
-    transferCost = ''
     nodeCost = 0
     centralCost = 0
 
     # Some houses have flexibility, and no deviation. Other houses have deviation, but no flex. Lets do 50/50
     # The for loop under is information gathering to the blockchain
-    for i in range(0,numHouses):
+    for i in range(numHouses):
+        if i % 2 == 0:
+            tx_hash = RealTime.functions.setRealTimeNodePrice(i, upPrice[1][i], downPrice[1][i]).transact({
+                'from': web3.eth.accounts[0],
+                'gas': 1000000,
+                'gasPrice': 0,
+                'nonce': web3.eth.get_transaction_count(web3.eth.accounts[0]),
+            })
+            receipt = wait_for_receipt(tx_hash)
+            updateCost.append(receipt.gasUsed)
+            print(f"Gas used for setting price for node {i}: {receipt.gasUsed}")
 
-        if (i % 2 == 0):
-            updateCost.append(RealTime.transact().setRealTimeNodePrice(i, upPrice[1][i], downPrice[1][i]))
-            updateCost.append(RealTime.transact().setRealTimeNodeBattery(i, upAvailableFlex[1][i], downAvailableFlex[1][i], 0))
-            numTxNodes = numTxNodes + 2
+            tx_hash = RealTime.functions.setRealTimeNodeBattery(i, upAvailableFlex[1][i], downAvailableFlex[1][i], 0).transact({
+                'from': web3.eth.accounts[0],
+                'gas': 1000000,
+                'gasPrice': 0,
+                'nonce': web3.eth.get_transaction_count(web3.eth.accounts[0]),
+            })
+            receipt = wait_for_receipt(tx_hash)
+            updateCost.append(receipt.gasUsed)
+            print(f"Gas used for setting battery for node {i}: {receipt.gasUsed}")
+            numTxNodes += 2
         else:
-            updateCost.append(RealTime.transact().setRealTimeNodePrice(i, 0, 0))
-            updateCost.append(RealTime.transact().setRealTimeNodeBattery(i, 0, 0, deviation[i]))
-            numTxNodes = numTxNodes + 2
+            tx_hash = RealTime.functions.setRealTimeNodePrice(i, 0, 0).transact({
+                'from': web3.eth.accounts[0],
+                'gas': 1000000,
+                'gasPrice': 0,
+                'nonce': web3.eth.get_transaction_count(web3.eth.accounts[0]),
+            })
+            receipt = wait_for_receipt(tx_hash)
+            updateCost.append(receipt.gasUsed)
+            print(f"Gas used for setting price for node {i}: {receipt.gasUsed}")
+
+            tx_hash = RealTime.functions.setRealTimeNodeBattery(i, 0, 0, deviation[i]).transact({
+                'from': web3.eth.accounts[0],
+                'gas': 1000000,
+                'gasPrice': 0,
+                'nonce': web3.eth.get_transaction_count(web3.eth.accounts[0]),
+            })
+            receipt = wait_for_receipt(tx_hash)
+            updateCost.append(receipt.gasUsed)
+            print(f"Gas used for setting battery for node {i}: {receipt.gasUsed}")
+            numTxNodes += 2
+
 
     # The for loop below is the central node fetching all information from the blockchain
     for i in range(0,numHouses):
-        h = RealTime.caller().getRealTimeNode(i)
+        h = RealTime.functions.getRealTimeNode(i).call()
         if (h[5] < 0):
             demand[0].append(i)
             demand[1].append(-h[5])
@@ -127,8 +185,8 @@ def trade(numTxCentral, numTxNodes, numTxInitiate, price, battery, availableFlex
         flexFlag = 2
         marketPrice = wholesalePrice
 
-    sortedPrice = [[0 for x in range(numHouses)] for y in range(2)]
-    copySortedPrice = [[0 for x in range(numHouses)] for y in range(2)]
+    sortedPrice = [[0 for _ in range(numHouses)] for y in range(2)]
+    copySortedPrice = [[0 for _ in range(numHouses)] for y in range(2)]
 
     # If supply and demand do not cancel each other, we must utilise the batteries to cancel the deviations
     i = 0
@@ -188,8 +246,16 @@ def trade(numTxCentral, numTxNodes, numTxInitiate, price, battery, availableFlex
     ################################################################################
     numTxCentral = len(transactions[0])
     print(len(transactions[0]), len(transactions[1]), len(transactions[2]))
-    transferCost = RealTime.transact().checkAndTransactList(firstFlexFlag, sortedPrice[0], sortedPrice[1], transactions[0], transactions[1], transactions[2], marketPrice, FlexCoin.address)
-    centralCost = web3.eth.getTransactionReceipt(transferCost).gasUsed
+    tx_hash = RealTime.functions.checkAndTransactList(firstFlexFlag, sortedPrice[0], sortedPrice[1], transactions[0], transactions[1], transactions[2], marketPrice, addressFlexCoin).transact({
+        'from': web3.eth.accounts[0],
+        'gas': 1000000,
+        'gasPrice': 0,
+        'nonce': web3.eth.get_transaction_count(web3.eth.accounts[0]),
+    })
+    receipt = wait_for_receipt(tx_hash)
+    transferCost = receipt.gasUsed
+    print(f"Gas used for check and transact list: {transferCost}")
+    centralCost = transferCost
 
     if (firstFlexFlag == 0):
         for i in range(numTransactionsFirstRound,len(transactions[0])):
@@ -198,43 +264,22 @@ def trade(numTxCentral, numTxNodes, numTxInitiate, price, battery, availableFlex
         for i in range(numTransactionsFirstRound,len(transactions[0])):
             battery[transactions[0][i]] = battery[transactions[0][i]] + transactions[2][i]
     for i in range(0, len(updateCost)):
-        nodeCost = web3.eth.getTransactionReceipt(updateCost[i]).gasUsed + nodeCost
-    #for i in range(0, len(createNodeCost)):
-    #    nodeCost = web3.eth.getTransactionReceipt(createNodeCost[i]).gasUsed + nodeCost
-
+        nodeCost += updateCost[i]
+    print(f"Total node cost: {nodeCost}")
     return(numTxCentral, numTxNodes, numTxInitiate, battery, marketPrice, nodeCost, centralCost)
 
 ######## Now, we could test the system over a certain set of time steps
 def testRealTime(numHouses, numPeriods):
-    i = 0
 
     #####################################################################################
     ####### The loop below is called if there not are premade sufficient houses in FlexCoin
     ####### If creates new nodes in the blockchain network, and giving them ether, which is necessary in order to trade
     ####### Thereafter, it is created a new house from its address
     #####################################################################################
-    # while (FlexCoin.FlexCoin.caller().numHouses() < numHouses):
-    #     web3.eth.personal.newAccount('pass') # Pass is the password necessary to unlock the account.
-    #     web3.eth.personal.unlockAccount(web3.eth.accounts[FlexCoin.FlexCoin.caller().numHouses()], 'pass')
-    #     web3.eth.sendTransaction({'to': web3.eth.accounts[FlexCoin.FlexCoin.caller().numHouses()], 'from': web3.eth.coinbase, 'value': 123456789})
-    #     ################# Now, the new node is created, and have an amount of ether. We must now create a house in his address
-    #     FlexCoin.FlexCoin.transact().newHouse({'from': web3.eth.accounts[FlexCoin.FlexCoin.caller().numHouses()]})
-
-    # ### The variables below is used to measure the amount of transactions which is done in the system
-    # ## Central is the central node, node is how many each node sends in, and initiate is when a new account is initiated.
-    # numTxCentral = 0
-    # numTxNodes = 0
-    # numTxInitiate = 0
-
-    # for i in range(0,numHouses):
-    #     createNodeCost.append(RealTime.transact().newRealTimeNode(web3.eth.accounts[i]))
-    #     numTxInitiate = numTxInitiate + 1
     numHouses = min(numHouses, len(web3.eth.accounts)) 
     numTxCentral = 0
     numTxNodes = 0
     numTxInitiate = 0
-
-    createNodeCost = []
 
     for i in range(numHouses):
         tx_hash = RealTime.functions.newRealTimeNode(web3.eth.accounts[i]).transact({
@@ -248,13 +293,13 @@ def testRealTime(numHouses, numPeriods):
         print(f"Gas used for creating RealTime node ({i + 1}): {receipt.gasUsed}")
         numTxInitiate += 1
 
-    deviation = [[0 for x in range(numHouses)] for y in range(numPeriods)]
-    battery = [[0 for x in range(numHouses)] for y in range(numPeriods)]
-    batteryFlag = [0 for x in range(0,numHouses)]
-    price = [[[0 for z in range(numHouses)] for x in range(0, 2)] for y in range(numPeriods)]
-    nodeCost = [0 for i in range(numPeriods)]
-    centralCost = [0 for i in range(numPeriods)]
-    flexCoinBalance = [[0 for x in range(numHouses)] for y in range(numPeriods)]
+    deviation = [[0 for _ in range(numHouses)] for _ in range(numPeriods)]
+    battery = [[0 for _ in range(numHouses)] for _ in range(numPeriods)]
+    batteryFlag = [0 for _ in range(0,numHouses)]
+    price = [[[0 for _ in range(numHouses)] for _ in range(0, 2)] for _ in range(numPeriods)]
+    nodeCost = [0 for _ in range(numPeriods)]
+    centralCost = [0 for _ in range(numPeriods)]
+    flexCoinBalance = [[0 for _ in range(numHouses)] for _ in range(numPeriods)]
 
     for i in range(0, numHouses):
         if (i % 2 == 0):
@@ -269,8 +314,8 @@ def testRealTime(numHouses, numPeriods):
             price[i][1][j] = random.randint(350, 460)
 
     for j in range(0,numHouses):
-        _, flexCoinBalance[0][j] = FlexCoin.FlexCoin.caller().getHouse(web3.eth.accounts[j])
-    marketPrice = [RealTime.caller().wholesalePrice() for x in range(0,numPeriods)]
+        _, flexCoinBalance[0][j] = FlexCoin.functions.getHouse(web3.eth.accounts[j]).call()
+    marketPrice = [RealTime.functions.wholesalePrice().call() for _ in range(numPeriods)]
 
     for i in range(0, numPeriods): #144 for a whole day
 
@@ -280,7 +325,7 @@ def testRealTime(numHouses, numPeriods):
                 battery[i][j] = battery[i - 1][j] + deviation[i][j]  # If deviation not is included, the batteries work without
                 # deviation[i][j] = 0 # It is being set to zero later anyway
             else:
-                if (deviation[i][j] < 0 and batteryFlag == 1):
+                if (deviation[i][j] < 0 and batteryFlag[j] == 1):
                     deviation[i][j] = deviation[i][j] + (13500 - battery[i][j])
                     battery[i][j] = 13500
                 else:
@@ -299,7 +344,7 @@ def testRealTime(numHouses, numPeriods):
         for j in range(0,numHouses):
             if (battery[i][j] > battery[i - 1][j]):
                 battery[i][j] = round(int(0.9 * battery[i][j]))
-            _, flexCoinBalance[i][j] = FlexCoin.FlexCoin.caller().getHouse(web3.eth.accounts[j])
+            _, flexCoinBalance[i][j] = FlexCoin.functions.getHouse(web3.eth.accounts[j]).call()
 
     averageNode = (sum(nodeCost)/numPeriods)
     averageCentral = sum(centralCost)/numPeriods
@@ -308,9 +353,9 @@ def testRealTime(numHouses, numPeriods):
 
 #### The simulation is now performed. The main results is averageCentral and averageNode, where these provides with the gas results from the central calculation and the nodes
 counter = 0
-node = [0 for i in testRange]
-total = [0 for i in testRange]
+node = [0 for _ in testRange]
+total = [0 for _ in testRange]
 for i in testRange:
     numTxCentral[counter], numTxNodes[counter], numTxInitiate[counter], flexCoinBalance, battery, price, deviation, marketPrice, averageNode[counter], averageCentral[counter] = testRealTime(i, 1)
     total[counter] = node[counter] + averageCentral[counter]
-    counter = counter + 1
+    counter += 1
